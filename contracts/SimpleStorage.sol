66e866956973c43aa6b04e8517ef66fefc287386
PhemeloDev <sspegah@gmail.com>
2024-10-15 07:32:08 +0200

    Initial Commit

---
+++ contracts/SimpleStorage.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.26;
+
+contract SimpleStorage {
+    //solidity types : typesboolean, uint , int, string, address, bytes
+
+    
+    uint256 favoriteNumber;
+
+
+}
