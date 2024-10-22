96c548c88a5ec457cd500062777719eb9cf5a2c1
PhemeloDev <sspegah@gmail.com>
2024-10-22 06:35:52 +0200

    Latest code

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -6,7 +6,17 @@ contract SimpleStorage {
 
     //0xd9145CCE52D386f254917e481eB44e9943F39138
 
-    uint256 public favoriteNumber;
+    uint256 public favoriteNumber;   
+
+    mapping(string => uint256)public nametoFavNumber;
+    mapping(uint256 => string)public favNumbertoName;
+
+    struct People{
+        uint256 favoriteNumber;
+        string name;
+    }
+
+    People[] public people;
 
     function store(uint256 _favoriteNumber) public {
         favoriteNumber = _favoriteNumber;
@@ -20,5 +30,11 @@ contract SimpleStorage {
 
     function retrieve() public view returns (uint256){
         return favoriteNumber;
+    }
+
+    function addPerson(uint256 _favoriteNumber, string memory _name) public {
+        people.push(People({favoriteNumber: _favoriteNumber, name: _name}));
+        nametoFavNumber[_name] = _favoriteNumber;
+        favNumbertoName[_favoriteNumber] = _name;
     }
 }
